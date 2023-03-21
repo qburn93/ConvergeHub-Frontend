053b31eb66fb1fc537e35f7da528b8be69974106
Bogdan <bogdan.gugu93@gmail.com>
2023-03-21 22:22:41 +0000

    Add icons, create navbarmodule css file with app module, create navbar with dropdown categories

---
+++ src/components/NavBar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
+
+const NavBar = () => {
+    return (
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
+            <Container>
+                <Navbar.Brand>
+                    <img src={logo} alt="logo" height="45" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto text-left">
+                        <Nav.Link>
+                            <i className="fas fa-home"></i>Home
+                        </Nav.Link>
+                        <Nav.Link>
+                            <i className="fas fa-sign-in-alt"></i>Sign in
+                        </Nav.Link>
+                        <Nav.Link>
+                            <i className="fas fa-user-plus"></i>Sign up
+                        </Nav.Link>
+                        <NavDropdown title="Categories" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Uncategorized</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Nature
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Adventure</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default NavBar;
