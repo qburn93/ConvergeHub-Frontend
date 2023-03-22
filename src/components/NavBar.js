19769c9b9d904e0e9845850256c5289f6a067126
Bogdan <bogdan.gugu93@gmail.com>
2023-03-22 22:04:18 +0000

    Wrap categories and current logged in user in loggedinIcons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
 
 
 const NavBar = () => {
+    const currentUser = useContext(CurrentUserContext)
+    const loggedInIcons = <>
+        {currentUser?.username}
+        <NavDropdown className={styles.NavLink} activeClassName={styles.Active} title="Categories" id="basic-nav-dropdown">
+                            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/uncategorized">Uncategorized</NavLink>
+                            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/nature">Nature</NavLink>
+                            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/adventure">Adventure</NavLink>
+                        
+                        </NavDropdown>
+    
+    </>
+    
+    const loggedOutIcons = <> <NavLink
+    className={styles.NavLink}
+    activeClassName={styles.Active} to="/signin">
+    <i className="fas fa-sign-in-alt"></i>Sign in
+</NavLink>
+<NavLink
+    exact
+    className={styles.NavLink}
+    activeClassName={styles.Active} to="/signup">
+    <i className="fas fa-user-plus"></i>Sign up
+</NavLink></>
+    
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
@@ -23,23 +48,8 @@ const NavBar = () => {
                             activeClassName={styles.Active} to="/">
                             <i className="fas fa-home"></i>Home
                         </NavLink>
-                        <NavLink
-                            className={styles.NavLink}
-                            activeClassName={styles.Active} to="/signin">
-                            <i className="fas fa-sign-in-alt"></i>Sign in
-                        </NavLink>
-                        <NavLink
-                            exact
-                            className={styles.NavLink}
-                            activeClassName={styles.Active} to="/signup">
-                            <i className="fas fa-user-plus"></i>Sign up
-                        </NavLink>
-                        <NavDropdown className={styles.NavLink} activeClassName={styles.Active} title="Categories" id="basic-nav-dropdown">
-                            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/uncategorized">Uncategorized</NavLink>
-                            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/nature">Nature</NavLink>
-                            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/adventure">Adventure</NavLink>
-
-                        </NavDropdown>
+                        
+                        {currentUser ? loggedInIcons : loggedOutIcons }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import axios from "axios"
+import React, { useContext, useState } from "react";
+import axios from "axios";
 
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -13,32 +13,41 @@ import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import appStyles from "../../App.module.css"
-
+import appStyles from "../../App.module.css";
+import { SetCurrentUserContext } from "../../App";
 
 function SignInForm() {
-    //   Add your component logic here
+    const setCurrentUser = useContext(SetCurrentUserContext)
+
+
+
     const [signInData, setSignInData] = useState({
         username: "",
         password: "",
     });
     const { username, password } = signInData;
 
+    const [errors, setErrors] = useState({});
+
     const history = useHistory();
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            await axios.post("/dj-rest-auth/login/", signInData);
+            const {data} = await axios.post("/dj-rest-auth/login/", signInData);
+            setCurrentUser(data.user)
             history.push("/");
         } catch (err) {
+            setErrors(err.response?.data);
         }
     };
+
     const handleChange = (event) => {
         setSignInData({
             ...signInData,
             [event.target.name]: event.target.value,
         });
     };
+
     return (
         <Row className={styles.Row}>
             <Col className="my-auto p-0 p-md-2" md={6}>
@@ -46,26 +55,26 @@ function SignInForm() {
                     <h1 className={styles.Header}>sign in</h1>
                     <Form onSubmit={handleSubmit}>
                         <Form.Group controlId="username">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control type="text"
+                            <Form.Label className="d-none">Username</Form.Label>
+                            <Form.Control
+                                type="text"
                                 placeholder="Username"
                                 name="username"
                                 className={styles.Input}
                                 value={username}
                                 onChange={handleChange}
                             />
-                            <Form.Text className="text-muted">
-                                We'll never share your credentials with anyone else.
-                            </Form.Text>
                         </Form.Group>
                         {errors.username?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
+                            <Alert key={idx} variant="warning">
                                 {message}
                             </Alert>
                         ))}
+
                         <Form.Group controlId="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password"
+                            <Form.Label className="d-none">Password</Form.Label>
+                            <Form.Control
+                                type="password"
                                 placeholder="Password"
                                 name="password"
                                 className={styles.Input}
@@ -74,14 +83,15 @@ function SignInForm() {
                             />
                         </Form.Group>
                         {errors.password?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
+                            <Alert key={idx} variant="warning">
                                 {message}
                             </Alert>
                         ))}
-
-                        <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
-                            type="submit">
-                            Login
+                        <Button
+                            className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+                            type="submit"
+                        >
+                            Sign in
                         </Button>
                         {errors.non_field_errors?.map((message, idx) => (
                             <Alert key={idx} variant="warning" className="mt-3">
@@ -89,7 +99,6 @@ function SignInForm() {
                             </Alert>
                         ))}
                     </Form>
-
                 </Container>
                 <Container className={`mt-3 ${appStyles.Content}`}>
                     <Link className={styles.Link} to="/signup">
