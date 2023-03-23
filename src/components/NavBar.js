7fba77a34ca252cd97542488351b8cf31bcb1675
Bogdan <bogdan.gugu93@gmail.com>
2023-03-23 21:19:02 +0000

    create custom hook for burger menu toggle hide

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,11 +9,15 @@ import {
 } from "../context/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
+
+
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
-
+    const { expanded, setExpanded, ref } = useClickOutsideToggle();
+    
     const handleSignOut = async () => {
         try {
             await axios.post("dj-rest-auth/logout/");
@@ -84,7 +88,7 @@ const NavBar = () => {
     );
 
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
             <Container>
                 <NavLink to="/">
                     <Navbar.Brand>
@@ -92,7 +96,10 @@ const NavBar = () => {
                     </Navbar.Brand>
                 </NavLink>
                 {currentUser && addPostIcon}
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle 
+                    ref={ref}
+                    onClick={() => setExpanded(!expanded)} 
+                    aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
                         <NavLink
